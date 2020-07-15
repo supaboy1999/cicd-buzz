9ec3c948c860dadfcd5fd916cbea53bae9953f51
supaboy1999 <supaboy1999@gmail.com>
2020-07-15 20:44:20 +0200

    changed info on system once again

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
     buzz = generator.generate_buzz()
     machine = platform.uname()
-    machine_text = "INFO system:"+machine.system+" node:"+machine.node+" relase:"+machine.release+" version:"+\
-                   machine.version+" machine:"+machine.machine+" processor:"+machine.processor
-    return render_template('index.html', buzz=buzz, machine=machine_text)
+    return render_template('index.html', buzz=buzz, machine=machine)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
