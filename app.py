513ff50f17a69b4c6450b1b4f89abb29c4d16107
supaboy1999 <supaboy1999@gmail.com>
2020-07-15 17:35:41 +0200

    added uname function and display

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import signal
+import platform
 from flask import Flask, render_template
 from buzz import generator
 
@@ -10,7 +11,10 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 @app.route("/")
 def generate_buzz():
     buzz = generator.generate_buzz()
-    return render_template('index.html', buzz=buzz)
+    machine = platform.uname()
+    machine_text = "sys:"+machine.system+" node:"+machine.node+" relase:"+machine.release+" version:"+\
+                   machine.version+" machine:"+machine.machine+" processor:"+machine.processor
+    return render_template('index.html', buzz=buzz, machine=machine_text)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
