62e525ac0d70d1ab866cf175784f66a3d459489d
supaboy1999 <supaboy1999@gmail.com>
2020-07-15 18:08:08 +0200

    changed info on system

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
     buzz = generator.generate_buzz()
     machine = platform.uname()
-    machine_text = "sys:"+machine.system+" node:"+machine.node+" relase:"+machine.release+" version:"+\
+    machine_text = "INFO system:"+machine.system+" node:"+machine.node+" relase:"+machine.release+" version:"+\
                    machine.version+" machine:"+machine.machine+" processor:"+machine.processor
     return render_template('index.html', buzz=buzz, machine=machine_text)
 
