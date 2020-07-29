fde776c1bcc3c89e031a96e8e437e725bdea151a
supaboy1999 <supaboy1999@gmail.com>
2020-07-29 15:19:53 +0200

    added simple form and POST method

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import signal
 import platform
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from buzz import generator
 
 app = Flask(__name__)
@@ -14,6 +14,14 @@ def generate_buzz():
     title = buzz[:16]
     machine = platform.uname()
     return render_template('index.html', buzz=buzz, machine=machine,title=title)
+@app.route('/', methods=['POST'])
+def generate_buzz_with_text():
+    text = request.form["text"]
+    text = text.upper()
+    buzz = generator.generate_buzz()
+    title = buzz[:16]
+    machine = platform.uname()
+    return render_template('index.html', buzz=buzz, machine=machine,title=title,text=text)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
